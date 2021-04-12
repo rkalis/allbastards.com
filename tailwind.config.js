10822c0dcbf6f7e239c08dcdb27d29c38f54695c
Rosco Kalis <roscokalis@gmail.com>
2021-04-11 22:34:33 -0500

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      charriot: ['Charriot Deluxe']
+    }
   },
   variants: {
     extend: {},
