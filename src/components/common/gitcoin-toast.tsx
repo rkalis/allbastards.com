d564eaeda0c618475d06dbf51e25a40faa0d5fc7
Rosco Kalis <roscokalis@gmail.com>
2021-09-27 16:24:28 +0200

    Disable gitcoin toast

--- src/components/common/gitcoin-toast.tsx
+++ src/components/common/gitcoin-toast.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { toast } from '../../utils';
-import Link from './Link';
+// import React from 'react';
+// import { toast } from '../../utils';
+// import Link from './Link';
 
 export const displayGitcoinToast = () => {
-  const toastContent = (
-    <div>
-      <div>
-        Hello friend! <span role="img" aria-label="wave">👋</span>
-      </div>
+  // const toastContent = (
+  //   <div>
+  //     <div>
+  //       Hello friend! <span role="img" aria-label="wave">👋</span>
+  //     </div>
 
-      <div>
-        Gitcoin Grants Round 11 is now live with $950k in donation matching!
-        Please consider supporting ALLBASTARDS.COM by donating to the <Link text="ALLBASTARDS.COM grant" to="https://gitcoin.co/grants/259/rosco-kalis-crypto-software-engineer" />
-      </div>
-    </div>
-  );
+  //     <div>
+  //       Gitcoin Grants Round 11 is now live with $950k in donation matching!
+  //       Please consider supporting ALLBASTARDS.COM by donating to the <Link text="ALLBASTARDS.COM grant" to="https://gitcoin.co/grants/259/rosco-kalis-crypto-software-engineer" />
+  //     </div>
+  //   </div>
+  // );
 
-  toast(toastContent, {
-    position: 'top-left',
-    autoClose: false,
-  });
+  // toast(toastContent, {
+  //   position: 'top-left',
+  //   autoClose: false,
+  // });
 };
