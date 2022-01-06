12ea0c6a187b98c2f5a9464d494c38702fa48028
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-06 15:08:17 -0500

    Changing logic to handle edge cases for bids

--- src/components/details/market/MarketDetails.tsx
+++ src/components/details/market/MarketDetails.tsx
@@ -1,6 +1,5 @@
 import { useWeb3React } from '@web3-react/core';
 import { providers } from 'ethers';
-import { toAddress } from '@rarible/types';
 import { ZERO_ADDRESS } from '../../../utils/constants';
 import { MarketData } from '../../../utils/interfaces';
 import { displayPrice, getBidPriceDisplay, getBidsFromAccount, getListingPriceDisplay } from '../../../utils/market';
@@ -21,14 +20,12 @@ interface Props {
 function MarketDetails({ marketData, refresh }: Props) {
   const { account, library } = useWeb3React<providers.Web3Provider>();
 
+  const activeAccountIsOwner = marketData.owner === account;
+  const isBidFromOwner = marketData.bids[0].maker.toLowerCase() === marketData.owner.toLowerCase();
+
   const listingPriceDisplay = getListingPriceDisplay(marketData.listings);
 
-  const activeAccountIsOwner = marketData.owner === account;
-
-  // Need to remove the current owners bids if they exist
-  const bidsOwnerRemoved = marketData.bids.filter((bid) => bid.maker !== toAddress(marketData.owner ?? ZERO_ADDRESS));
-
-  const bidPriceDisplay = getBidPriceDisplay(bidsOwnerRemoved);
+  const bidPriceDisplay = getBidPriceDisplay(marketData.bids);
   const activeBidsFromUser = getBidsFromAccount(marketData.bids, account ?? ZERO_ADDRESS);
   const inactiveBidsFromUser = getBidsFromAccount(marketData.inactiveBids, account ?? ZERO_ADDRESS);
   const bidsFromUser = [...activeBidsFromUser, ...inactiveBidsFromUser];
@@ -39,25 +36,30 @@ function MarketDetails({ marketData, refresh }: Props) {
   const canSell = library !== undefined && activeAccountIsOwner;
   const canBid = library !== undefined && account !== undefined && !activeAccountIsOwner;
   const canBuy = canBid && isForSale;
-  const canAcceptBid = canSell && hasBid;
+  const canAcceptBid = canSell && hasBid && !isBidFromOwner;
 
   return (
     <div>
       <h3 className="font-bold text-center text-2xl md:text-3xl">MARKET</h3>
       <div className="flex flex-col gap-2">
         <div className="w-1/2 max-w-md mx-auto text-center text-sm md:text-base">
-          <div>This bastard is currently owned by {marketData.ownerDisplay}.</div>
+          {activeAccountIsOwner && <div>You currently own this bastard!</div>}
+          {!activeAccountIsOwner && <div>This bastard is currently owned by {marketData.ownerDisplay}.</div>}
+
           {listingPriceDisplay && <div>This bastard is currently for sale for {listingPriceDisplay}.</div>}
           {!listingPriceDisplay && <div>This bastard is currently not for sale.</div>}
+
           {bidPriceDisplay && <div>There is a bid of {bidPriceDisplay} on this bastard.</div>}
           {!bidPriceDisplay && <div>There are currently no active bids on this bastard.</div>}
+
           {
-            canBid && hasBidsFromUser &&
+            hasBidsFromUser &&
               <div>
                 You have {bidsFromUser.length} bid(s) on this bastard of {bidsFromUser.map((bid) => displayPrice(bid, 'make')).join(', ')}.
               </div>
           }
-          {(!canBid || !hasBidsFromUser) && <div>You have no bids on this bastard.</div>}
+          {!hasBidsFromUser && <div>You have no bids on this bastard.</div>}
+
         </div>
         <div className="flex justify-center gap-2">
           {canSell && !isForSale && <Sell marketData={marketData} refresh={refresh} />}
@@ -66,7 +68,7 @@ function MarketDetails({ marketData, refresh }: Props) {
           {canBuy && <Buy marketData={marketData} refresh={refresh} />}
           {canBid && !hasBidsFromUser && <Bid marketData={marketData} refresh={refresh} />}
           {canBid && hasBidsFromUser && <UpdateBid bids={bidsFromUser} refresh={refresh} />}
-          {canBid && hasBidsFromUser && <CancelBids bids={bidsFromUser} refresh={refresh} />}
+          {hasBidsFromUser && <CancelBids bids={bidsFromUser} refresh={refresh} />}
           {canAcceptBid && <AcceptBid marketData={marketData} refresh={refresh} />}
         </div>
       </div>
